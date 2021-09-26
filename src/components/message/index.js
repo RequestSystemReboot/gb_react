48f6a4d579d2be8afe3e98e912cacb4f67d6782c
bryzgalin <bryzgalin@mac>
2021-09-27 00:30:16 +0300

    hw2 - chat

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./message.module.css";
 
-export const Message = (props) => {
-
-    return <p className={styles.letters}>{props.children}</p>
-
-}
+export const Message = ({ text, author, date, color }) => {
+  return (
+    <div className={styles.MessageDiv} style={{ color: color }}>
+      <p className={styles.Text}>{text}</p>
+      <p className={styles.Attrs}>
+        {author} {new Date(date).toLocaleTimeString("ru-Ru")}
+      </p>
+    </div>
+  );
+};
