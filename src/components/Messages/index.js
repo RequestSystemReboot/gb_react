2022c468176a2c591db2f82fd5053c1199a84655
bryzgalin <bryzgalin@mac>
2021-10-02 23:35:58 +0300

    hw3 - Material UI + ChatList

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { List, ListItem } from "@material-ui/core";
+import { List } from "@material-ui/core";
 import { Message } from "../Message";
 
 export const Messages = ({ messages }) => {
   return (
     <List sx={{ width: "100%", height: "85vh", overflow: "auto" }}>
       {messages.map((message) => (
-        <ListItem key={message.id}>
           <Message
             text={message.text}
             author={message.author}
@@ -14,7 +13,6 @@ export const Messages = ({ messages }) => {
             date={message.date}
             key={message.id}
           />
-        </ListItem>
       ))}
     </List>
   );
