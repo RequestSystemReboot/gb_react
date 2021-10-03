7956b8e2121050dfeedd84684fe73c8fc45a719a
bryzgalin <bryzgalin@mac>
2021-10-03 23:24:22 +0300

    hw4 - Router

--- src/App.js
+++ src/App.js
@@ -1,91 +1,43 @@
-import styles from "./App.module.css";
-import { useEffect, useState } from "react";
-import { MessageInput } from "./components/MessageInput";
+import { useState } from "react";
 import faker from "faker";
-import {
-  Container,
-  FormControlLabel,
-  FormGroup,
-  Grid,
-  Switch,
-} from "@material-ui/core";
+
 import { Layout } from "./components/Layout";
-import { ChatList } from "./components/ChatList";
-import { Messages } from "./components/Messages";
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "./routes/Routes";
 
 const createChat = () => ({
   id: faker.datatype.uuid(),
-  name: faker.animal.dog(),
+  name: faker.vehicle.manufacturer(),
 });
 
 function App() {
-  const [MessageList, setMessageList] = useState([]);
-  const [ChatItems] = useState(Array.from({ length: 25 }).map(createChat));
+  const [ChatItems, setChatItems] = useState(Array.from({ length: 7 }).map(createChat));
   const [DarkTheme, setDarkTheme] = useState(false);
 
-  function SendMessage(text, author, color) {
-    const date = new Date().getTime();
-    let message_list = [...MessageList];
-    message_list.push({
-      text: text,
-      author: author,
-      date: date,
-      id: author + date, // no key error
-      color: color,
-    });
-    setMessageList(message_list);
+  function AddChat() {
+    let chats = [...ChatItems]
+    chats.push(createChat())
+    setChatItems(chats)
   }
 
-  function Robot(text) {
-    setTimeout(
-      SendMessage,
-      1500,
-      "'" + text + "' - Got it, bro!",
-      "Robot",
-      "darkred"
-    );
+  function DeleteChat() {
+    let chats = [...ChatItems]
+    chats.pop()
+    setChatItems(chats)
   }
-
-  useEffect(() => {
-    if (MessageList.length > 0) {
-      const last_message = MessageList[MessageList.length - 1];
-      if (last_message.author === "Human") {
-        Robot(last_message.text);
-      }
-    }
-  }, [MessageList]);
 
   return (
-    <Layout darktheme={DarkTheme}>
-      <Container fixed>
-        <Grid container spacing={1} className={styles.App}>
-          <Grid item xs={3} className={styles.ChatList}>
-            <ChatList ChatItems={ChatItems} />
-          </Grid>
-          <Grid item xs={9} className={styles.Messages}>
-            <Messages messages={MessageList} />
-          </Grid>
-          <Grid item xs={10} className={styles.Input}>
-            <MessageInput onSend={SendMessage} />
-          </Grid>
-          <Grid item xs={2}>
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Switch
-                    defaultChecked
-                    onChange={() => {
-                      setDarkTheme(!DarkTheme);
-                    }}
-                  />
-                }
-                label="Theme"
-              />
-            </FormGroup>
-          </Grid>
-        </Grid>
-      </Container>
-    </Layout>
+    <BrowserRouter>
+      <Layout darktheme={DarkTheme}>
+        <Routes
+          ChatItems={ChatItems}
+          setDarkTheme={setDarkTheme}
+          DarkTheme={DarkTheme}
+          AddChat={AddChat}
+          DeleteChat={DeleteChat}
+        />
+      </Layout>
+    </BrowserRouter>
   );
 }
 
---
+++ src/navigation/index.js
@@ -0,0 +1,13 @@
+const composePath = (...args) =>
+  args
+    .map((item) => (typeof item === "function" ? item() : item.toString()))
+    .join("/");
+
+export const getHomePath = () => "";
+
+export const getProfilePath = () => "/profile";
+
+export const getChatsPath = () => composePath(getHomePath(), "chats");
+
+export const getChatByIdPath = (chatId = ":chatId") =>
+  composePath(getChatsPath(), chatId);
