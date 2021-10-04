2231a4857144cfed948f30bfe86671caaaa4bd71
bryzgalin <bryzgalin@mac>
2021-10-04 14:53:12 +0300

    hw5 - Redux

--- src/routes/Profile/index.js
+++ src/routes/Profile/index.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { Checkbox, Grid } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import { SAMPLE_CHECK } from "../../components/Redux/profile/actionTypes";
 
 export const Profile = () => {
-  return <div>Profile</div>;
+  const dispatch = useDispatch();
+  const ticked = useSelector((state) => {
+    return state.ticked
+  });
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <h2>Profile</h2>
+      </Grid>
+      <Grid item xs={12}>
+        <Checkbox
+          checked={ticked}
+          onChange={() => {
+            dispatch({
+              type: SAMPLE_CHECK,
+            });
+          }}
+        >
+          Sample
+        </Checkbox>
+      </Grid>
+    </Grid>
+  );
 };
